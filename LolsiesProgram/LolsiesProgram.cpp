9178fc296a601dde32446f94d9e159adb2f797c1
sabianroberts <djdise@outlook.com>
2021-01-09 19:28:56 +0000

    Add project files.

---
+++ LolsiesProgram/LolsiesProgram.cpp
@@ -0,0 +1,31 @@
+// DOESN'T WORK????? YOU NEED C++ REDISTS LOLOLOLOLOLOLOLOLOLOLOLLLOLOLOLOLOLOLOL
+// https://www.techpowerup.com/download/visual-c-redistributable-runtime-package-all-in-one/ download it now twat
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <stdbool.h>
+#include <tchar.h>
+int main()
+{
+	SetConsoleTitle(_T("TWAT"));
+	std::string FannyTron69 = "No one fucking cares about VSCode mate. It's just a fancy way to look cool with your web development shit. You could just use Notepad, or even GitHub online, but no. You just have to use VSCode and tell people not to use Wix because it makes yourself look cool and shit. 'Ew, you use Wix? Mate I could code a website better than that in 10 mins.' Mate, your website is literally just your YouTube and an advert for Old Digibox. I bet you can't make it anything better than that without stealing code from my website or reading 10 year old StackOverflow comments and calling it yours.";
+	std::string BorisJohnsonFartDetector = "This is a personal message from Boris Johnson UK Prime Minister: I can smell your egg farts, you will be moved up to Tier 5 (in beta mode ssh) and I will personally have you arrested. Git fuck";
+
+	std::cout << FannyTron69 << "\n";
+	system("pause");
+	std::cout << BorisJohnsonFartDetector << "\n";
+	system("pause");
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
