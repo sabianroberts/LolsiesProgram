11908a6eb07535ef8ef5444fe957c6d322948ab4
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2021-12-11 00:50:48 +0000

    Update mate...

--- LolsiesProgram/LolsiesProgram.cpp
+++ LolsiesProgram/LolsiesProgram.cpp
@@ -8,13 +8,13 @@
 #include <tchar.h>
 int main()
 {
-	SetConsoleTitle(_T("TWAT"));
-	std::string FannyTron69 = "No one fucking cares about VSCode mate. It's just a fancy way to look cool with your web development shit. You could just use Notepad, or even GitHub online, but no. You just have to use VSCode and tell people not to use Wix because it makes yourself look cool and shit. 'Ew, you use Wix? Mate I could code a website better than that in 10 mins.' Mate, your website is literally just your YouTube and an advert for Old Digibox. I bet you can't make it anything better than that without stealing code from my website or reading 10 year old StackOverflow comments and calling it yours.";
-	std::string BorisJohnsonFartDetector = "This is a personal message from Boris Johnson UK Prime Minister: I can smell your egg farts, you will be moved up to Tier 5 (in beta mode ssh) and I will personally have you arrested. Git fuck";
+	SetConsoleTitle(_T("Alexander Boris de Pfeffel Johnson."));
+	std::string GoodEvening = "Good evening. The coronavirus is the biggest threat this country has faced in decades.";
+	std::string BorisJohnsonFartDetector = "I can smell your egg farts, you will be moved up to Tier 5 (in beta mode ssh) and I will personally have you arrested. Git fuck";
 	std::string BigFatBruv = "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. Ding  A single lap should be completed each time you hear this sound. Ding  Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, ding ";
 	std::string f = "That's it. I can't take it anymore. Believe it or not, I am a virus; and I've just had a looksee at your computer. What is THAT in your Pictures folder? WTF bro? I have suddenly decided that I want to die. Goodbye.";
 
-	std::cout << FannyTron69 << "\n";
+	std::cout << GoodEvening << "\n";
 	system("pause");
 	std::cout << BorisJohnsonFartDetector << "\n";
 	system("pause");
